ef68d6c11c44a49b3475e86490f3d67124ae999b
Aaron <michaelproudfoot@ymail.com>
2023-04-24 20:16:40 -0700

    connected backand front

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+BACKEND_URL = "http://127.0.0.1:5000"
+@app.get("/")
+def index():
+    timestamp = datetime.now().strftime("%F %H:%M:%S")
+    return render_template("index.html", ts=timestamp)
+
+@app.get("/about")
+def about_me():
+    me = {
+        "first_name": "Aaron",
+        "last_name": "Pride",
+        "hobbies": "Creating"
+    }
+    return render_template("about.html", about=me)
+
+@app.get("/tasks")
+def display_all_tasks():
+    url = "%s/%s" % (BACKEND_URL, "tasks")
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        task_data = resp.json()
+        return render_template("task_list.html", tasks=task_data["tasks"])
+    return render_template("error.html", err_code=resp.status_code), resp.status_code
